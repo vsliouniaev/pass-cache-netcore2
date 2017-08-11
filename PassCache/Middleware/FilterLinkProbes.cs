76b29cadec5aadd60f78d19204b6b3d8583b7033
vas <v.sliouniaev@gmail.com>
2017-08-11 10:44:05 +0100

    Porting over from old project

---
+++ PassCache/Middleware/FilterLinkProbes.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace PassCache.Middleware
+{
+    public class FilterLinkProbes
+    {
+        private static readonly string[] BlockedAgents = { "skype", "whatsapp" };
+        private readonly RequestDelegate _next;
+
+        public FilterLinkProbes(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Headers.TryGetValue("User-Agent", out StringValues userAgents))
+            {
+                if (userAgents.Select(a => a.ToLowerInvariant()).Any(userAgent => BlockedAgents.Any(blockedAgent => blockedAgent.Contains(userAgent))))
+                {
+                    context.Response.Redirect("/", true);
+                    return;
+                }
+            }
+
+            await _next(context);
+        }
+    }
+}
